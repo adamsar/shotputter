8b9aa929abe9006c0e8cad41da76e5f87e93d3d6
Andrew Adams <adamsar@gmail.com>
2020-01-17 17:35:49 +0900

    Proper controls

--- src/main/ts/components/canvas/EditorCanvas.tsx
+++ src/main/ts/components/canvas/EditorCanvas.tsx
@@ -10,13 +10,20 @@ interface EditorCanvasProps {
 export const EditorCanvas = observer<EditorCanvasProps>(() => {
     const { screenshot, tools } = useStores();
     const [canvas, setCanvas] = React.useState<fabric.Canvas>(null);
+    const onAddObject = (e: fabric.IEvent) => {
+        e.target.hasBorders = e.target.hasControls = e.target.evented = tools.currentTool !== "draw";
+    }
 
     React.useEffect(() => {
         const canvasElement = document.getElementById("shotput-canvas-container");
         if (canvasElement.children.length === 0 && !canvas) {
             screenshot.screenshotCanvas.id = "shotput-canvas";
             canvasElement.appendChild(screenshot.screenshotCanvas);
-            setCanvas(new fabric.Canvas('shotput-canvas'));
+            const _canvas = new fabric.Canvas('shotput-canvas');
+            _canvas.on({
+                "object:added": onAddObject
+***REMOVED***);
+            setCanvas(_canvas);
         }
     });
 
@@ -28,9 +35,11 @@ export const EditorCanvas = observer<EditorCanvasProps>(() => {
                     break;
 
                 case "shape":
+                    canvas.isDrawingMode = false;
                     break;
 
                 case "text":
+                    canvas.isDrawingMode = false;
                     break;
 
                 default:
