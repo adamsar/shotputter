1bac5575841d469f144b5a8adbc357d58895fad0
Andrew Adams <adamsar@gmail.com>
2020-02-10 15:39:33 +0900

    Poster modal

--- src/main/ts/services/poster/Poster.ts
+++ src/main/ts/services/poster/Poster.ts
@@ -1,8 +1,11 @@
 import {Post} from "./Post";
 import {PostResult} from "./PostResult";
 
+export type PosterType = "download";
+
 export interface Poster {
-    
+
+    typeName: PosterType;
     send(post: Post): Promise<PostResult>;
 
 }
--- src/main/ts/stores/ScreenshotStore.ts
+++ src/main/ts/stores/ScreenshotStore.ts
@@ -2,11 +2,14 @@ import {observable} from "mobx";
 import html2canvas from "html2canvas";
 import {MAIN_ID} from "../constants";
 import {Post} from "../services/poster/Post";
+import {Poster} from "../services/poster/Poster";
+import {DownloadPoster} from "../services/poster/DownloadPoster";
 
 export class ScreenshotStore {
 
     @observable screenshotCanvas: HTMLCanvasElement | null = null;
     @observable post: Post | null = null;
+    @observable availablePosters: Poster[] = [DownloadPoster(document)];
 
     async takeScreenshot() {
         const canvas = await html2canvas(document.body, {
