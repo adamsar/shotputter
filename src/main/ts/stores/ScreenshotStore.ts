ad762a03f12a67209a006cab58a0ee6c6de4e9a9
Andrew Adams <adamsar@gmail.com>
2020-02-18 14:42:11 +0900

    Remove unnecessary files

--- src/main/ts/stores/ScreenshotStore.ts
+++ src/main/ts/stores/ScreenshotStore.ts
@@ -9,38 +9,6 @@ import {SlackPoster} from "../services/poster/slack/SlackPoster";
 import {GithubPoster} from "../services/poster/github/GithubPoster";
 import {ImgurUploader} from "../services/images/imgur";
 
-// @ts-ignore
-function screenshot(element, options = {}) {
-    // our cropping context
-    let cropper = document.createElement('canvas').getContext('2d');
-    // save the passed width and height
-    // @ts-ignore
-    let finalWidth = options.width || window.innerWidth;
-    // @ts-ignore
-    let finalHeight = options.height || window.innerHeight;
-    // update the options value so we can pass it to h2c
-    // @ts-ignore
-    if (options.x) {
-        // @ts-ignore
-        options.width = finalWidth + options.x;
-    }
-    // @ts-ignore
-    if (options.y) {
-        // @ts-ignore
-        options.height = finalHeight + options.y;
-    }
-    // chain h2c Promise
-    return html2canvas(element, options).then(c => {
-        // do our cropping
-        cropper.canvas.width = finalWidth;
-        cropper.canvas.height = finalHeight;
-        // @ts-ignore
-        cropper.drawImage(c, -(+options.x || 0), -(+options.y || 0));
-        // return our canvas
-        return cropper.canvas;
-    });
-}
-
 export class ScreenshotStore {
 
     @observable screenshotCanvas: HTMLCanvasElement | null = null;
@@ -51,7 +19,10 @@ export class ScreenshotStore {
     slackPoster: SlackPoster | null = null;
 
     constructor(appOptions: AppOptions) {
-        const availablePosters = [DownloadPoster(document)];
+        const availablePosters = [];
+        if (appOptions.download !== false) {
+            availablePosters.push(DownloadPoster(document));
+        }
         if (appOptions.slack?.token) {
             this.slackPoster = SlackPoster({...appOptions.slack, channel: appOptions.slack.channel || ""})
             availablePosters.push(this.slackPoster);
@@ -69,11 +40,11 @@ export class ScreenshotStore {
 
     async takeScreenshot() {
         const canvas = await html2canvas(document.body, {
-                        height: window.innerHeight,
-                        width: window.innerWidth,
-                        y: window.scrollY,
-                        ignoreElements: element => element.id === MAIN_ID,
-        ***REMOVED***);
+            height: window.innerHeight,
+            width: window.innerWidth,
+            y: window.scrollY,
+            ignoreElements: element => element.id === MAIN_ID,
+***REMOVED***
         this.screenshotCanvas = document.createElement("canvas");
         this.screenshot = canvas.toDataURL("image/jpeg");
     }
@@ -84,6 +55,7 @@ export class ScreenshotStore {
 
     resetCanvas() {
         this.screenshotCanvas = null;
+        this.screenshot = null;
         this.post = null;
     }
 
