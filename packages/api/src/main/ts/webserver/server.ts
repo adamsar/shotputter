c9cb5d8d8fe8b1ab9cb4e844de58b11a1e461af4
Andrew Adams <adamsar@gmail.com>
2020-09-04 13:32:17 +0900

    Fix server bootstrap

--- packages/api/src/main/ts/webserver/server.ts
+++ packages/api/src/main/ts/webserver/server.ts
@@ -1,5 +1,4 @@
-import {Express} from "express";
-import express from "express";
+import express, {Express} from "express";
 import {slackRouter} from "./slack/slack-router";
 import {githubRouter} from "./github/github-router";
 import * as es6Promise from "es6-promise";
@@ -16,7 +15,10 @@ import {googleRouter} from "./google/google-router";
 import {getJiraRouter} from "./jira/jira-router";
 import {CloudinaryUploader} from "./cloudinary/CloudinaryImageUploader";
 import {CustomerImageUploader} from "@shotputter/common/src/main/ts/services/images/custom-uploader";
-import {ImageArchiver, LocalImageArchiver} from "./local_files/LocalImageArchiver";
+import {LocalImageArchiver} from "./local_files/LocalImageArchiver";
+import chalk from "chalk";
+import {isLeft} from "fp-ts/lib/Either";
+
 const cors = require("cors");
 
 es6Promise.polyfill();
@@ -34,8 +36,6 @@ export interface SlackServerConfig {
 export interface GithubServerConfig {
     enabled: boolean;
     token: string;
-    defaultOwner?: string;
-    defaultRepo?: string;
 }
 
 export interface ImgurServerConfig {
@@ -94,81 +94,174 @@ export interface CustomImageUploaderConfig {
     enabled: boolean;
     endpoint: string;
 }
+const enabled = chalk.green("Enabled");
+const disabled = chalk.red("Disabled");
+const needImageUploader = chalk.yellow("image uploader required")
 
 export const getApp = (serverConfig: ServerConfig = {}): Express => {
     const app = express();
-    const enabledPosters = [];
-    let imgurUploader: ImageUploader;
-    let s3Uploader: ImageUploader;
-    let cloudinaryUploader: ImageUploader;
-    let customUploader: ImageUploader;
-    let localImageUploader: ImageArchiver;
+    let imageUploader: ImageUploader;
+    const updates: string[] = [];
+    let imageUploaderName: string;
+    const services: string[] = [];
 
     app.use(express.json({ limit: "10mb" }));
     app.use(cors());
-    if (serverConfig.files?.enabled) {
-        localImageUploader = LocalImageArchiver(
-            serverConfig.files.directory,
-            serverConfig.files.host
-        );
-        localImageUploader.testWrite()().then(console.log)
-        app.use("/images", express.static(serverConfig.files.directory));
-        console.log("Serving images from " + serverConfig.files.directory);
-    }
-    if (serverConfig.slack?.token) {
-        console.log("Slack enabled");
-        app.use("/slack", slackRouter(serverConfig.slack));
-        enabledPosters.push("slack");
-    } else {
-        console.warn("Slack client id is not configured. Not using Slack integration.")
-    }
-    if (serverConfig.s3?.enabled) {
-        console.log("Using S3 uploads");
-        s3Uploader = S3Images(
-            process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION,
-            serverConfig.s3.bucket,
-            undefined,
-            serverConfig.s3.prefix)
-    }
-    if (serverConfig.imgur?.clientId) {
-        console.log("Using Imgur");
-        imgurUploader = ImgurUploader(serverConfig.imgur.clientId)
-    }
-    if (serverConfig.cloudinary?.enabled && serverConfig.cloudinary?.apiKey && serverConfig.cloudinary?.apiSecret && serverConfig.cloudinary?.cloudName) {
-        console.log("Using Cloudinary");
-        cloudinaryUploader = CloudinaryUploader(serverConfig.cloudinary?.cloudName, serverConfig.cloudinary?.apiKey, serverConfig.cloudinary?.apiSecret);
-    }
-    if (serverConfig.customImageUploader?.enabled && serverConfig.customImageUploader?.endpoint) {
-        customUploader = CustomerImageUploader(serverConfig.customImageUploader?.endpoint);
-    }
-    const uploader = localImageUploader || customUploader || imgurUploader || s3Uploader || cloudinaryUploader;
 
-    if (serverConfig.github?.token && uploader) {
-        console.log("Github enabled");
-        app.use("/github", githubRouter(serverConfig.github, uploader));
-        enabledPosters.push("github");
+    if (serverConfig.imgur?.enabled) {
+        if (serverConfig.imgur.clientId) {
+            updates.push(`Imgur:\t${enabled}`);
+            imageUploader = ImgurUploader(serverConfig.imgur.clientId);
+            imageUploaderName = "imgur";
+        } else {
+            updates.push(`Imgur:\t${disabled}\t${chalk.yellow("Client ID needed")}`);
+        }
     } else {
-        if (uploader) {
-            console.warn("No image uploader configured");
-        } else {
-            console.warn("Github id not configured. Not using Github integration.")
-        }
+        updates.push(`Imgur:\t${disabled}`);
     }
-    if (serverConfig.google?.webhookUrl && serverConfig.google?.enabled) {
-        if (uploader) {
-            console.log("Google enabled")
-            app.use("/google", googleRouter(serverConfig.google?.webhookUrl, uploader));
+
+    if (serverConfig.cloudinary?.enabled) {
+        if (serverConfig.cloudinary.cloudName) {
+            if (serverConfig.cloudinary.apiKey) {
+                if (serverConfig.cloudinary.apiSecret) {
+                    imageUploader = CloudinaryUploader(serverConfig.cloudinary?.cloudName, serverConfig.cloudinary?.apiKey, serverConfig.cloudinary?.apiSecret);
+                    updates.push(`Cloudinary:\t${enabled}`);
+                    imageUploaderName = "Cloudinary";
+                } else {
+                    updates.push(`Cloudinary:\t${disabled}\t${chalk.yellow("apiSecret required")}`);
+                }
+            } else {
+                updates.push(`Cloudinary:\t${disabled}\t${chalk.yellow("apiKey required")}`);
+            }
         } else {
-            console.warn("Google chat enabled, but no image poster was registered! Not using Google chat")
+            updates.push(`Cloudinary:\t${disabled}\t${chalk.yellow("cloudName required")}`);
         }
+    } else {
+        updates.push(`Cloudinary:\t${disabled}`)
     }
-    if (serverConfig.jira?.enabled && serverConfig.jira.username && serverConfig.jira.password) {
-        if (uploader) {
-            console.log("JIRA enabled");
-            app.use("/jira", getJiraRouter(serverConfig.jira, uploader))
+
+    if (serverConfig.s3?.enabled) {
+        if (serverConfig.s3.bucket) {
+            imageUploader = S3Images(
+                process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION,
+                serverConfig.s3.bucket,
+                undefined,
+                serverConfig.s3.prefix)
+            imageUploaderName = "S3"
+            updates.push(`S3:\t${enabled}`);
         } else {
-            console.log("JIRA enabled, but no image poster was registered! Not using JIRA");
+            updates.push(`S3:\t${disabled}\t${chalk.yellow("Bucket required")}`)
         }
+    } else {
+        updates.push(`S3:\t${disabled}`);
     }
+
+    if (serverConfig.files?.enabled) {
+        if (serverConfig.files.directory) {
+            if (serverConfig.files.host) {
+                const archiver = LocalImageArchiver(
+                    serverConfig.files.directory,
+                    serverConfig.files.host
+                );
+                imageUploader = archiver;
+                imageUploaderName = "LocalFiles";
+                updates.push(`LocalFiles:\t${enabled}\t${serverConfig.files.directory}`);
+                archiver.testWrite()().then(result => {
+                    if (isLeft(result)) {
+                        console.error(result.left);
+                        console.log("Exiting...");
+                        process.exit(2);
+                    }
+                });
+            } else {
+                updates.push(`LocalFiles:\t${disabled}\t${chalk.yellow("host required")}`);
+            }
+        } else {
+            updates.push(`LocalFiles:\t${disabled}\t${chalk.yellow("directory required")}`);
+        }
+    } else {
+        updates.push(`LocalFiles:\t${disabled}`);
+    }
+    if (serverConfig.customImageUploader?.enabled) {
+        if (serverConfig.customImageUploader.endpoint) {
+            imageUploaderName = "CustomImages";
+            imageUploader = CustomerImageUploader(serverConfig.customImageUploader?.endpoint);
+            updates.push(`CustomImages:\t${enabled}`);
+        } else {
+            updates.push(`CustomImages:\t${disabled}\tendpoint required`);
+        }
+    } else {
+        updates.push(`CustomImages:\t${disabled}`);
+    }
+
+    if (serverConfig.slack?.enabled) {
+        if (serverConfig.slack.token) {
+            app.use("/slack", slackRouter(serverConfig.slack));
+            updates.push(`Slack:\t${enabled}`);
+            services.push("Slack");
+        } else {
+            updates.push(`Slack:\t${disabled}\t${chalk.yellow("token needed")}`);
+        }
+    } else {
+        updates.push(`Slack:${disabled}`)
+    }
+
+    if (serverConfig.github?.enabled) {
+        if (serverConfig.github.token) {
+            if (imageUploader) {
+                app.use("/github", githubRouter(serverConfig.github, imageUploader));
+                updates.push(`Github:\t${enabled}`);
+                services.push("Github");
+            } else {
+                updates.push(`Github:\t${disabled}\t${needImageUploader}`)
+            }
+        } else {
+            updates.push(`Github:\t${disabled}\t${chalk.yellow("token required")}`)
+        }
+    } else {
+        updates.push(`Github:\t${disabled}`);
+    }
+    if (serverConfig.google?.enabled) {
+        if (serverConfig.google.webhookUrl) {
+            if (imageUploader) {
+                updates.push(`Google:\t${enabled}`);
+                app.use("/google", googleRouter(serverConfig.google?.webhookUrl, imageUploader));
+                services.push("Google")
+            } else {
+                updates.push(`Google:\t${disabled}\t${needImageUploader}`);
+            }
+        } else {
+            updates.push(`Google:\t${disabled}\t${chalk.yellow("webhookUrl required")}`);
+        }
+    } else {
+        updates.push(`Google:\t${disabled}`);
+    }
+
+    if (serverConfig.jira?.enabled) {
+        if (serverConfig.jira.username) {
+            if (serverConfig.jira.password) {
+                if (serverConfig.jira.host) {
+                    if (imageUploader) {
+                        app.use("/jira", getJiraRouter(serverConfig.jira, imageUploader));
+                        updates.push(`Jira:\t${enabled}`);
+                        services.push("Jira");
+                    } else {
+                        updates.push(`Jira:\t${disabled}\t${needImageUploader}`);
+                    }
+                } else {
+                    updates.push(`Jira:\t${disabled}\t${chalk.yellow("host required")}`);
+                }
+            } else {
+                updates.push(`Jira:\t${disabled}\t${chalk.yellow("password required")}`);
+            }
+        } else {
+            updates.push(`Jira:\t${disabled}\t${chalk.yellow("username required")}`);
+        }
+    } else {
+        updates.push(`Jira:\t${disabled}`);
+    }
+    console.log(updates.join("\n"));
+    console.log(`Image uploader: ${imageUploaderName ?? "None"}`)
+    console.log(`Services available: ${services.join(", ")}`);
     return app;
 };
